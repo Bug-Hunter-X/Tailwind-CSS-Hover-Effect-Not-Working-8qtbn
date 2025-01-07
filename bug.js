10b5a7829273ac92295b46a1f3491b82d16d6100
BugHunter-X <askzenai@gmail.com>
2025-01-07 17:18:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+```javascript
+// Some Tailwind CSS classes are not applied correctly.
+
+<div class="bg-red-500 hover:bg-blue-700 p-4">
+  Hover me!
+</div>
+
+// The hover effect doesn't work.
+```
